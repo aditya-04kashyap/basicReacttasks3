e83345fef764930c55300ab432a5f8a838ea4ea6
aditya-04kashyap <kashyap1aditya@gmail.com>
2023-10-09 02:18:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ColorPicker from "./ColorPicker";
+import "./App.css";
 
 function App() {
+  const colors = [
+    "#FF5733",
+    "#33FF57",
+    "#5733FF",
+    "#FF3399",
+    "#33FFFF",
+    "#FFD700",     // Gold
+    "#7CFC00",     // Lawn Green
+    "#8A2BE2",     // Blue Violet
+    "#FFA07A",     // Light Salmon
+    "#00BFFF",     // Deep Sky Blue
+    "cyan",        // Cyan
+    "slateblue",   // Slate Blue
+    "#FF5733",     // Red
+    "#FF4500",     // OrangeRed
+    "#FF6347",     // Tomato
+    "#FF7F50",     // Coral
+    "#FF8C00",     // Dark Orange
+    "#33FF57",     // Lime Green
+    "#32CD32",     // Lime
+    "#00FF00",     // Green
+    "#4169E1",     // Royal Blue
+    "#0000FF",     // Blue
+    "#6495ED",     // Cornflower Blue
+    "#808080",     // Gray
+    "#D3D3D3",     // Light Gray
+    "#A9A9A9",     // Dark Gray
+    "#B0E0E6",     // Powder Blue
+    "#ADD8E6",     // Light Blue
+    "#4682B4",     // Steel Blue
+    "#5F9EA0",     // Cadet Blue
+    "#2F4F4F",     // Dark Slate Gray
+    "#708090",     // Slate Gray
+    "#708090",     // Zinc
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center><h1>Color Picker App</h1>
+      <ColorPicker colors={colors} />
+      </center>
     </div>
   );
 }
